6094e3b7d10d8d1ffb12dbc24692eec7ce46a346
Nasrullahmalik <130395171+Nasrullahmalik@users.noreply.github.com>
2023-12-03 15:48:16 +0500

    Add files via upload

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,101 +1,109 @@
-import ytdl from 'youtubedl-core';
-import yts from 'youtube-yts';
-import fs from 'fs';
-import { pipeline } from 'stream';
-import { promisify } from 'util';
-import os from 'os';
+import fetch from 'node-fetch';
 import axios from 'axios';
-
-const streamPipeline = promisify(pipeline);
-
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `Use example ${usedPrefix}${command} naruto blue bird`;
-  await m.react(rwait);
-
-  try {
-    // Encode the query for the API request
-    const query = encodeURIComponent(text);
-
-    // Make a GET request to the API
-    const response = await axios.get(`${gurubot}/ytsearch?text=${query}`);
-    const result = response.data.results[0]; // Get the first result
-
-    if (!result) throw 'Video Not Found, Try Another Title';
-
-    // Extract video information from the API response
-    const { title, thumbnail, duration, views, uploaded, url } = result;
-
-    // Create a message caption with video information
-    const captvid = `✼ ••๑⋯ ❀ Y O U T U B E ❀ ⋯⋅๑•• ✼
-  ❏ Title: ${title}
-  ❐ Duration: ${duration}
-  ❑ Views: ${views}
-  ❒ Upload: ${uploaded}
-  ❒ Link: ${url}
-⊱─━━━━⊱༻●༺⊰━━━━─⊰`;
-
-    // Send the video information along with the thumbnail to the Discord channel
-    conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author }, { quoted: m });
-
-    // Download and send the audio of the video
-    const audioStream = ytdl(url, {
-      filter: 'audioonly',
-      quality: 'highestaudio',
-    });
-
-    // Get the path to the system's temporary directory
-    const tmpDir = os.tmpdir();
-
-    // Create a writable stream in the temporary directory
-    const writableStream = fs.createWriteStream(`${tmpDir}/${title}.mp3`);
-
-    // Start the download
-    await streamPipeline(audioStream, writableStream);
-
-    // Prepare the message document with audio file and metadata
-    const doc = {
-      audio: {
-        url: `${tmpDir}/${title}.mp3`
-      },
-      mimetype: 'audio/mpeg',
-      ptt: false,
-      waveform: [100, 0, 0, 0, 0, 0, 100],
-      fileName: `${title}`,
-      contextInfo: {
-        externalAdReply: {
-          showAdAttribution: true,
-          mediaType: 2,
-          mediaUrl: url,
-          title: title,
-          body: 'HERE IS YOUR SONG',
-          sourceUrl: url,
-          thumbnail: await (await conn.getFile(thumbnail)).data
-        }
-      }
-    };
-
-    // Send the audio message to the Discord channel
-    await conn.sendMessage(m.chat, doc, { quoted: m });
-
-    // Delete the downloaded audio file
-    fs.unlink(`${tmpDir}/${title}.mp3`, (err) => {
-      if (err) {
-        console.error(`Failed to delete audio file: ${err}`);
-      } else {
-        console.log(`Deleted audio file: ${tmpDir}/${title}.mp3`);
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    throw 'An error occurred while searching for YouTube videos.';
+import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
+import fs from "fs";
+import yts from 'yt-search';
+let limit1 = 100;
+let limit2 = 400;
+let limit_a1 = 50;
+let limit_a2 = 400;
+const handler = async (m, {conn, command, args, text, usedPrefix}) => {
+  if (!text) throw `_*<Mr-Malik - play />*_ \ n \ n*[ℹ️] The title or link of the YouTube video is needed.*\ N \ n*[💡] Example: Mera dil badaldy_`;    const yt_play = await search(args.join(' '));
+    let additionalText = '';
+    if (command === 'play') {
+      additionalText = 'audio';
+    } else if (command === 'play2') {
+      additionalText = 'vídeo';
+    }
+    const texto1 = `_*< Mr-Malik - PLAY />*_\n\n▢ *Títle:* ${yt_play[0].title}\n\n▢ *Publised:* ${yt_play[0].ago}\n\n▢ *Duration:* ${secondString(yt_play[0].duration.seconds)}\n\n▢ *Views:* ${`${MilesNumber(yt_play[0].views)}`}\n\n▢ *Author:* ${yt_play[0].author.name}\n\n▢ *ID:* ${yt_play[0].videoId}\n\n▢ *Type:* ${yt_play[0].type}\n\n▢ *Enlace:* ${yt_play[0].url}}\n\n*[ ℹ️ ]  ${additionalText}. espere...*`.trim();
+    conn.sendMessage(m.chat, {image: {url: yt_play[0].thumbnail}, caption: texto1}, {quoted: m});
+    if (command == 'play') {
+    try {   
+    const audio = global.API('ApiEmpire', `/api/v1/ytmp3?url=${yt_play[0].url}`)
+    const ttl = await yt_play[0].title
+    const buff_aud = await getBuffer(audio);
+    const fileSizeInBytes = buff_aud.byteLength;
+    const fileSizeInKB = fileSizeInBytes / 1024;
+    const fileSizeInMB = fileSizeInKB / 1024;
+    const size = fileSizeInMB.toFixed(2);       
+    if (size >= limit_a2) {  
+    await conn.sendMessage(m.chat, {text: `*[ ✔ ] Downloaded successfully ${audio}*`}, {quoted: m});
+    return;    
+    }     
+    if (size >= limit_a1 && size <= limit_a2) {  
+    await conn.sendMessage(m.chat, {document: buff_aud, mimetype: 'audio/mpeg', fileName: ttl + `.mp3`}, {quoted: m});   
+    return;
+    } else {
+    await conn.sendMessage(m.chat, {audio: buff_aud, mimetype: 'audio/mpeg', fileName: ttl + `.mp3`}, {quoted: m});   
+    return;    
+    }} catch {
+    throw '_*< Mr-Malik - PLAY />*_\n\n*[ ℹ️ ] error . Please try again later.*';    
+    }}
+    if (command == 'play2') {
+    try {   
+    const video = global.API('ApiEmpire', `/api/v1/ytmp4?url=${yt_play[0].url}`)
+    const ttl2 = await yt_play[0].title
+    const buff_vid = await getBuffer(video);
+    const fileSizeInBytes2 = buff_vid.byteLength;
+    const fileSizeInKB2 = fileSizeInBytes2 / 1024;
+    const fileSizeInMB2 = fileSizeInKB2 / 1024;
+    const size2 = fileSizeInMB2.toFixed(2);       
+    if (size2 >= limit2) {  
+    await conn.sendMessage(m.chat, {text: `_*< Mr-Malik - PLAY />*_\n\n*[ ✔ ] Video Downloaded ${video}*`}, {quoted: m});
+    return;    
+    }     
+    if (size2 >= limit1 && size2 <= limit2) {  
+    await conn.sendMessage(m.chat, {document: buff_vid, mimetype: 'video/mp4', fileName: ttl2 + `.mp4`}, {quoted: m});   
+    return;
+    } else {
+    await conn.sendMessage(m.chat, {video: buff_vid, mimetype: 'video/mp4', fileName: ttl2 + `.mp4`}, {quoted: m});   
+    return;    
+    }} catch {
+    throw '_*< Mr-Malik - PLAY />*_\n\n*[ ℹ️ ] error.try again later.*';    
+    }
   }
 };
-
-handler.help = ['play'].map((v) => v + ' <query>');
-handler.tags = ['downloader'];
-handler.command = /^play$/i;
-
-handler.exp = 0;
-
+handler.command = /^(play|play2)$/i;
 export default handler;
 
+async function search(query, options = {}) {
+  const search = await yts.search({query, hl: 'en', gl: 'EN', ...options});
+  return search.videos;
+}
+
+function MilesNumber(number) {
+  const exp = /(\d)(?=(\d{3})+(?!\d))/g;
+  const rep = '$1.';
+  const arr = number.toString().split('.');
+  arr[0] = arr[0].replace(exp, rep);
+  return arr[1] ? arr.join('.') : arr[0];
+}
+
+function secondString(seconds) {
+  seconds = Number(seconds);
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  const dDisplay = d > 0 ? d + (d == 1 ? 'd ' : 'd ') : '';
+  const hDisplay = h > 0 ? h + (h == 1 ? 'h ' : 'h ') : '';
+  const mDisplay = m > 0 ? m + (m == 1 ? 'm ' : 'm ') : '';
+  const sDisplay = s > 0 ? s + (s == 1 ? 's' : 's') : '';
+  return dDisplay + hDisplay + mDisplay + sDisplay;
+}
+
+function bytesToSize(bytes) {
+  return new Promise((resolve, reject) => {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return 'n/a';
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+    if (i === 0) resolve(`${bytes} ${sizes[i]}`);
+    resolve(`${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`);
+  });
+}
+
+const getBuffer = async (url, options) => {
+    options ? options : {};
+    const res = await axios({method: 'get', url, headers: {'DNT': 1, 'Upgrade-Insecure-Request': 1,}, ...options, responseType: 'arraybuffer'});
+    return res.data;
+};
