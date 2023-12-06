47c921d2a829cea12cb22e2c9396d0e7f9359913
Nasrullahmalik <130395171+Nasrullahmalik@users.noreply.github.com>
2023-12-06 16:53:52 +0500

    Add files via upload

--- plugins/aimalik_bard.js
+++ plugins/aimalik_bard.js
@@ -1,62 +1,60 @@
-import uploadImage from '../lib/uploadImage.js';
-import Bard from '../lib/bard2.js';
-
-const bardAi = new Bard();
-
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  let textInput;
-  
-  if (args.length >= 1) {
-    textInput = args.slice(0).join(" ");
+import _0x2d65cc from '../lib/uploadImage.js';
+import _0x37e01c from '../lib/bard.js';
+const bardAi = new _0x37e01c();
+let handler = async (_0x4be7c9, {
+  conn: _0x5ef915,
+  args: _0x477776,
+  usedPrefix: _0x1954a4,
+  command: _0x4fd041
+}) => {
+  let _0x2d15fc;
+  if (_0x477776.length >= 0x1) {
+    _0x2d15fc = _0x477776.slice(0x0).join(" ");
   } else {
-    if (m.quoted && m.quoted.text) {
-      textInput = m.quoted.text;
+    if (_0x4be7c9.quoted && _0x4be7c9.quoted.text) {
+      _0x2d15fc = _0x4be7c9.quoted.text;
     } else {
-      return m.reply("Give some text");
+      return _0x4be7c9.reply("Give some text");
     }
   }
-
-  let quotedMessage = m.quoted ? m.quoted : m;
-  let mimeType = (quotedMessage.msg || quotedMessage).mimetype || '';
-
-  await m.react('💸');
-
-  if (!mimeType) {
+  let _0x40ed32 = _0x4be7c9.quoted ? _0x4be7c9.quoted : _0x4be7c9;
+  let _0x31ff36 = (_0x40ed32.msg || _0x40ed32).mimetype || '';
+  await _0x4be7c9.react('💸');
+  if (!_0x31ff36) {
     try {
-      let result = await Bard(textInput);
-      await m.reply(result.content);
-    } catch (error) {
-      throw "An error occurred";
+      let _0x2e87ed = await Bard(_0x2d15fc);
+     const mea_0 = await _0x2e87ed.content;
+const mea = mea_0 +"\n\n*Google Bard AI*\nCreated by:\nNasrullah/923494757886";
+  _0x4be7c9.reply(mea);
+    } catch (_0x5d43cb) {
+      throw "An error occured";
     }
   } else {
-    let downloadedImage = await quotedMessage.download();
-    let isImage = /image\/(png|jpe?g)/.test(mimeType);
-
-    if (isImage) {
-      let processedImage = await uploadImage(downloadedImage);
-      let bardResult = await BardImg(textInput, processedImage);
-      await m.reply(bardResult.content);
+    let _0x16e1ad = await _0x40ed32.download();
+    let _0x4a18d4 = /image\/(png|jpe?g)/.test(_0x31ff36);
+    if (_0x4a18d4) {
+      let _0x2c3508 = await _0x2d65cc(_0x16e1ad);
+      let _0x17024e = await BardImg(_0x2d15fc, _0x2c3508);
+      const mea_0 = await _0x17024e.content ;
+     const mea = mea_0 +"\n\n*Google Bard AI*\nCreated by:\nNasrullah/923494757886";
+       _0x4be7c9.reply(mea);
     } else {
-      await m.reply("Only images are supported");
+      await _0x4be7c9.reply("Only images are supported");
     }
   }
 };
-
-handler.help = ["bard2"];
-handler.tags = ['ai2'];
-handler.command = /^(bard2)$/i;
-
+handler.help = ["barda"];
+handler.tags = ['ai22'];
+handler.command = /^(barda)$/i;
 export default handler;
-
-async function Bard(text) {
+async function Bard(_0x43fecc) {
   return await bardAi.question({
-    'ask': text
+    'ask': _0x43fecc
   });
-}
-
-async function BardImg(text, image) {
+};
+async function BardImg(_0x44c94d, _0x2b7bfe) {
   return await bardAi.questionWithImage({
-    'ask': text,
-    'image': image
+    'ask': _0x44c94d,
+    'image': _0x2b7bfe
   });
-}
+};
