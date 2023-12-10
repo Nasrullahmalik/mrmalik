adec6c74b4f812f233ac2b1559a0e87573f08c66
Malik The King <130395171+Nasrullahmalik@users.noreply.github.com>
2023-12-10 19:04:42 +0500

    Add files via upload

---
+++ plugins/yts-list.js
@@ -0,0 +1,42 @@
+import yts from 'yt-search';
+import fs from 'fs';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+  if (!text) throw `*type video/audio song name *\n\n*—◉ Example:*\n*${usedPrefix + command} Dil dil pakistan*`;
+  try {
+    const vids_ = {
+      from: m.sender,
+      urls: [],
+    };
+    if (!global.videoList) {
+      global.videoList = [];
+    }
+    if (global.videoList[0]?.from == m.sender) {
+      global.videoList.splice(0, global.videoList.length);
+    }
+    const results = await yts(text);
+    const textoInfo = `*\n کسی بھی نمبر کے ساتھ جواب دیں۔ طریقہ جاننے کے لئے نیچے والی مثال دیکھیں\n*
+◉ ${usedPrefix}a (for audio)
+◉ ${usedPrefix}b (for video)
+
+*—◉ Example:*
+*◉ ${usedPrefix}a 1*
+*◉ ${usedPrefix}b 1*`.trim();
+    const teks = results.all.map((v, i) => {
+      const link = v.url;
+      vids_.urls.push(link);
+      return `[${i + 1}] ${v.title}
+↳ 🫐 *_Link :_* ${v.url}
+↳ 🕒 *_Duración :_* ${v.timestamp}
+↳ 📥 *_Subido :_* ${v.ago}
+↳ 👁 *_Vistas :_* ${v.views}`;
+    }).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
+    conn.sendFile(m.chat, results.all[0].thumbnail, 'yts.jpeg', textoInfo + '\n\n' + teks, m);
+    global.videoList.push(vids_);
+  } catch {
+    await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾 𝙲𝙾𝙽 𝙾𝚃𝚁𝙾 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*');
+  }
+};
+handler.help = ['y *<texto>*'];
+handler.tags = ['search'];
+handler.command = /^y|playlist$/i;
+export default handler;
